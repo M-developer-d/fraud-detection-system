0b632445e73e61b0b7e8f506047c86b31ce84c2e
M-develoer-d <dukaremohan100@gmail.com>
2025-09-25 18:41:09 +0530

    added a mysql_data.py

---
+++ Fraud-Detection.py
@@ -0,0 +1,49 @@
+
+import mysql.connector
+import numpy as np
+from datetime import datetime
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="MOHAN@14_2007",
+    database="FRAUD_DETECTION"
+)
+cursor = conn.cursor(dictionary=True)
+
+cursor.execute("SELECT * FROM transactions")
+transactions = cursor.fetchall()
+
+amounts = np.array([t['amount'] for t in transactions])
+mean_amt = np.mean(amounts)
+std_amt = np.std(amounts)
+
+threshold = 3
+
+alerts = []
+
+for t in transactions:
+    z_score = (t['amount'] - mean_amt) / std_amt
+    if abs(z_score) > threshold:
+        reason = f"High transaction amount (Z-score={z_score:.2f})"
+        alerts.append((t['transaction_id'], t['user_id'], t['amount'], reason, datetime.now()))
+
+sql_insert = """
+INSERT INTO fraud_alerts (transaction_id, user_id, ammount, reason, created_at)
+VALUES (%s, %s, %s, %s, %s)
+"""
+
+if alerts:
+    cursor.executemany(sql_insert, alerts)
+    conn.commit()
+    print(f"✅ {len(alerts)} suspicious transactions flagged!")
+    print("Details:")
+    for alert in alerts:
+        print(f"Transaction ID: {alert[0]},\nUser ID: {alert[1]},\nAmount: {alert[2]},\nReason: {alert[3]} ")
+
+else:
+    print("No suspicious transactions detected.")
+
+# Close connection
+cursor.close()
+conn.close()
---
+++ MySQL_data.py
@@ -0,0 +1,53 @@
+import mysql.connector
+from faker import Faker
+import random
+
+# Setup Faker
+fake = Faker()
+
+# MySQL connection
+conn = mysql.connector.connect(
+    host="localhost",   
+    user="root",        
+    password="MOHAN@14_2007", 
+    database="FRAUD_DETECTION"
+)
+cursor = conn.cursor()
+cursor.execute("SELECT MAX(transaction_id) FROM transactions")
+max_id = cursor.fetchone()[0]
+if max_id is None:
+    max_id = 0
+
+def generate_transactions(n=100):
+    transactions = []
+    for i in range(n):
+        transaction_id = max_id + i + 1
+        user_id = random.randint(1, 10)  
+        amount = round(random.uniform(5, 5000), 2) 
+        timestamp = fake.date_time_between(start_date="-30d", end_date="now")
+        location = fake.city()
+        device_id = f"DEV-{random.randint(1000,9999)}"
+        
+        transactions.append((transaction_id, user_id, amount, timestamp, location, device_id))
+    return transactions
+
+try:
+    transactions = generate_transactions(100)
+    
+    sql = """
+    INSERT INTO transactions (transaction_id, user_id, amount, timestamp, location, device_id)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+    
+    cursor.executemany(sql, transactions)
+    conn.commit()
+    print(f"{cursor.rowcount} transactions inserted successfully!")
+    
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+    conn.rollback()
+    
+finally:
+    # Close connection
+    cursor.close()
+    conn.close()
